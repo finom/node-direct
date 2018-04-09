5ce0d4626348934d2a43d4a2003870bce16a379e
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2018-04-09 23:03:53 +0300

    fix: Extended bodyparser

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ if (cluster.isWorker) {
 
     const app = express();
 
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(middleware);
 
